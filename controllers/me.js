b41d7f32d48730d8d381485619f6bd1f94c182b5
Cyril Lazar <klazar@mail.ru>
2023-01-03 23:47:44 +1000

    fix: reviewer's issues

--- controllers/me.js
+++ controllers/me.js
@@ -1,39 +1,48 @@
 const User = require('../models/user');
 const NoExistError = require('../utils/NoExistError');
-const { defaultErrorStatus, dataErrorStatus, notFoundStatus } = require('../constants/errorStatuses');
+const {
+  defaultErrorStatus,
+  dataErrorStatus,
+  notFoundStatus,
+} = require('../constants/errorStatuses');
+
+function handleError(err, req, res) {
+  if (err.name === 'CastError') {
+    return res
+      .status(dataErrorStatus)
+      .send({ message: `Передан некорректный _id: ${req.user._id}` });
+  }
+
+  if (err instanceof NoExistError) {
+    return res
+      .status(notFoundStatus)
+      .send({
+        message: err.message,
+      });
+  }
+
+  if (err.name === 'ValidationError') {
+    return res.status(dataErrorStatus).send({
+      message: 'Переданы некорректные данные.',
+    });
+  }
+  return res
+    .status(defaultErrorStatus)
+    .send({ message: `Что-то пошло не так: ${err.name}` });
+}
 
 const changeUserData = (req, res) => {
-  User.findByIdAndUpdate(
-    req.user._id,
-    { ...req.body },
-    {
-      new: true, // обработчик then получит на вход обновлённую запись
-      runValidators: true, // данные будут валидированы перед изменением
-    },
-  )
+  User.findByIdAndUpdate(req.user._id, req.body, {
+    new: true, // обработчик then получит на вход обновлённую запись
+    runValidators: true, // данные будут валидированы перед изменением
+  })
     .then((user) => {
       if (user) {
         return res.send({ data: user });
       }
-      return Promise.reject(new NoExistError());
+      return Promise.reject(new NoExistError(`Пользователь с указанным _id: ${req.user._id} не найден.`));
     })
-    .catch((err) => {
-      if (err.name === 'CastError' || err instanceof NoExistError) {
-        res
-          .status(notFoundStatus)
-          .send({ message: `Пользователь с указанным _id: ${req.user._id} не найден.` });
-        return;
-      }
-      if (err.name === 'ValidationError') {
-        res
-          .status(dataErrorStatus)
-          .send({
-            message: 'Переданы некорректные данные при обновлении профиля.',
-          });
-        return;
-      }
-      res.status(defaultErrorStatus).send({ message: `Что-то пошло не так: ${err.name}` });
-    }); // Обработка ошибки
+    .catch((err) => handleError(err, req, res)); // Обработка ошибки
 };
 
 const changeUserAvatar = (req, res) => {
@@ -51,25 +60,9 @@ const changeUserAvatar = (req, res) => {
       if (user) {
         return res.send({ data: user });
       }
-      return Promise.reject(new NoExistError());
+      return Promise.reject(new NoExistError(`Пользователь с указанным _id: ${req.user._id} не найден.`));
     })
-    .catch((err) => {
-      if (err.name === 'CastError' || err instanceof NoExistError) {
-        res
-          .status(notFoundStatus)
-          .send({ message: `Пользователь с указанным _id: ${req.user._id} не найден.` });
-        return;
-      }
-      if (err.name === 'ValidationError') {
-        res
-          .status(dataErrorStatus)
-          .send({
-            message: 'Переданы некорректные данные при обновлении аватара.',
-          });
-        return;
-      }
-      res.status(defaultErrorStatus).send({ message: `Что-то пошло не так: ${err.name}` });
-    }); // Обработка ошибки
+    .catch((err) => handleError(err, req, res)); // Обработка ошибки
 };
 
 module.exports = { changeUserData, changeUserAvatar };
--- routers/errPath.js
+++ routers/errPath.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const errPath = require('../controllers/errPath');
 
-router.patch('/', errPath);
+router.all('/', errPath);
 
 module.exports = router;
