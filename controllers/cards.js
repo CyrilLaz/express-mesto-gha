6078aa4ebe0bc01f0a07f742c174fbc496496d4a
Cyril Lazar <klazar@mail.ru>
2023-02-05 16:52:09 +1000

    feat: контроллер для выдачи последних 18ти карточек

--- controllers/cards.js
+++ controllers/cards.js
@@ -8,6 +8,14 @@ const findAllCards = (req, res, next) => {
     .catch(next);
 };
 
+const findEighteenLastCards = (req, res, next) => {
+  Card.find({})
+    .sort('-createdAt')
+    .limit(18)
+    .then((data) => res.send({ data }))
+    .catch(next);
+};
+
 const createCard = (req, res, next) => {
   const { name, link } = req.body;
   const owner = req.user._id;
@@ -20,24 +28,32 @@ const removeCard = (req, res, next) => {
   Card.isOwnerCheck(req.params.cardId, req.user._id)
     .then((owned) => {
       if (!owned) {
-        return Promise.reject(new NoRightError('Удалить карточку может только владелец'));
+        return Promise.reject(
+          new NoRightError('Удалить карточку может только владелец')
+        );
       }
-      return Card.findByIdAndRemove(req.params.cardId)
-        .then(() => res.send({ message: 'Карточка удалена' }));
-    }).catch(next);
+      return Card.findByIdAndRemove(req.params.cardId).then(() =>
+        res.send({ message: 'Карточка удалена' })
+      );
+    })
+    .catch(next);
 };
 
 const putLike = (req, res, next) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
-    { new: true },
+    { new: true }
   )
     .then((card) => {
       if (card) {
         return res.send({ data: card });
       }
-      return Promise.reject(new NoExistError(`Передан несуществующий _id: ${req.params.cardId} карточки.`));
+      return Promise.reject(
+        new NoExistError(
+          `Передан несуществующий _id: ${req.params.cardId} карточки.`
+        )
+      );
     })
     .catch(next); // Обработка ошибки;
 };
@@ -46,13 +62,17 @@ const removeLike = (req, res, next) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } },
-    { new: true },
+    { new: true }
   )
     .then((card) => {
       if (card) {
         return res.send({ data: card });
       }
-      return Promise.reject(new NoExistError(`Передан несуществующий _id: ${req.params.cardId} карточки.`));
+      return Promise.reject(
+        new NoExistError(
+          `Передан несуществующий _id: ${req.params.cardId} карточки.`
+        )
+      );
     })
     .catch(next); // Обработка ошибки;
 };
@@ -63,4 +83,5 @@ module.exports = {
   removeCard,
   putLike,
   removeLike,
+  findEighteenLastCards,
 };
